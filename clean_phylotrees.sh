469717952208885d92b2f6d4d6318bf2c241c5d6
Logan Whitehouse <lswhiteh@live.unc.edu>
2020-05-12 09:48:29 -0400

    Update clean_phylotrees.sh

--- clean_phylotrees.sh
+++ clean_phylotrees.sh
@@ -1,2 +1,2 @@
-for f in *.fa.treefile; do cat ${f} | sed -e 's/\,/\,\n/g' | sed -e 's/_sp.*:/:/' | sed -e 's/prot.*:/prot:/' | sed -e 's/_tr.*:/:/' |  tr -d '\n' > ${f}.trimmed; done
+for f in *fa.treefile; do cat ${f} | sed -e 's/\,/\,\n/g'| sed -e 's/)/)\n/g' | sed -e 's/_sp.*:/:/' | sed -e 's/prot.*:/prot:/' | sed -e 's/_tr.*:/:/' |  tr -d '\n' > ${f}.trimmed; done
 
