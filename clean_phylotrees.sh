d69f28505b937759fa66a6916478313217e31736
Logan Whitehouse <lswhiteh@live.unc.edu>
2020-05-12 09:49:23 -0400

    Update clean_phylotrees.sh

--- clean_phylotrees.sh
+++ clean_phylotrees.sh
@@ -1,2 +1,3 @@
+#Unwraps tree by commas and end parens, then removes all inserted IDs by orthofinder for each line, wraps back up into completed tree
 for f in *fa.treefile; do cat ${f} | sed -e 's/\,/\,\n/g'| sed -e 's/)/)\n/g' | sed -e 's/_sp.*:/:/' | sed -e 's/prot.*:/prot:/' | sed -e 's/_tr.*:/:/' |  tr -d '\n' > ${f}.trimmed; done
 
