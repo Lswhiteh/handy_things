0641228a60569dcc519819fd07b2fc2b51d30fd9
Logan Whitehouse <lswhiteh@live.unc.edu>
2020-05-12 15:40:50 -0400

    Update comma-sep-list.sh

--- comma-sep-list.sh
+++ comma-sep-list.sh
@@ -1,2 +1,2 @@
-#Creates comma separated list of entries that match the grep
+#Creates comma separated list of entries that match the grep, in this case it's for NGS read filenames
 ls -p | grep R2 | tr '\n' ','
