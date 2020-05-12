6aaf81d24d99f7e7f0948894e64082cee650af42
Logan Whitehouse <lswhiteh@live.unc.edu>
2020-05-12 11:23:52 -0400

    Update clean_phylotrees.sh

--- clean_phylotrees.sh
+++ clean_phylotrees.sh
@@ -2,4 +2,25 @@
 for f in *fa.treefile; do cat ${f} | sed -e 's/\,/\,\n/g'| sed -e 's/)/)\n/g' | sed -e 's/_sp.*:/:/' | sed -e 's/prot.*:/prot:/' | sed -e 's/_tr.*:/:/' |  tr -d '\n' > ${f}.trimmed; done
 
 #Unwraps to get names on each line, gets number of duplicate species (paralog entries) 
-cat OG0006422.fa.treefile.trimmed | sed -e 's/\,/\,\n/g'| sed -e 's/)/)\n/g' | sed -e 's/(/(\n/g' | sed -e 's/:/\n/' | sort | uniq -c | sort -nr | awk 'NR==1{ print $1; }'
+for f in ./*trimmed
+do
+        if [[ $(cat ${f} | \
+                sed -e 's/\,/\,\n/g' | \
+                sed -e 's/)/\n/g' | \
+                sed -e 's/(/\n/g' | \
+                sed -e 's/:/\n/' | \
+                sed -e '/[0-9]/d' | \
+                sed -e '/\;/d' | \
+                sed -e '/^$/d' | \
+                sort | uniq -c | \
+                sort -nr | \
+                awk 'NR==1{ print $1; }') == '1' ]];
+
+        then
+
+                cp ${f} ../no_paras
+
+        fi
+
+done
+
